d19a65378fa73462c8d8c6ce842010b53edb3637
PeterFareed <46231250+PeterFareed@users.noreply.github.com>
2019-02-12 23:49:14 +0200

    Add files via upload

---
+++ Mnshn.js
@@ -0,0 +1,28 @@
+﻿
+Save New Duplicate & Edit Just Text Twitter
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+client.on("message", msg => {
+let ownerid = '537824006150291466'/
+if(msg.content === `<@${ownerid}>`){
+bot.users.get(ownerid).send(`
+في واحد منشنك
+
+In server: :arrow_down:
+\`${msg.guild.name}\`
+ 
+By: :arrow_down:
+***${msg.author}***`);
+}
+})
